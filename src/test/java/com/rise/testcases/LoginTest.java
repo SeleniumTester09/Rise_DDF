213f5f38589e679dfbde2e54d1c7744606d99d40
w8 <w8@Balas>
2018-05-13 15:02:48 +0530

    Commit_One

---
+++ src/test/java/com/rise/testcases/LoginTest.java
@@ -0,0 +1,25 @@
+package com.rise.testcases;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.rise.base.TestBase;
+
+public class LoginTest extends TestBase{
+	
+	@Test
+	public void loginAsBankManager() throws InterruptedException
+	{
+		log.debug("Inside Login Test");
+		driver.findElement(By.xpath(OR.getProperty("bmbtn"))).click();
+		log.debug("Navigated to Bank Manager Page");
+		Thread.sleep(3000); //Explicit wait for a while
+		log.debug(" Login successful");
+		
+		Assert.assertTrue(isElementPresent(By.xpath(OR.getProperty("addcust"))));
+		
+		
+	}
+
+}
